58e64fe2321a943c95ba87623d93ddb88c9f871f
Darby Lim <thlim@robotis.com>
2022-10-03 04:29:23 +0900

    Modified params

--- turtlebot3_cartographer/launch/cartographer.launch.py
+++ turtlebot3_cartographer/launch/cartographer.launch.py
@@ -1,4 +1,6 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
+#!/usr/bin/env python3
+#
+# Copyright 2022 ROBOTIS CO., LTD.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,75 +16,83 @@
 #
 # Author: Darby Lim
 
-import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    turtlebot3_cartographer_prefix = get_package_share_directory('turtlebot3_cartographer')
-    cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
-                                                  turtlebot3_cartographer_prefix, 'config'))
-    configuration_basename = LaunchConfiguration('configuration_basename',
-                                                 default='turtlebot3_lds_2d.lua')
+    start_rviz = LaunchConfiguration('start_rviz')
+    use_sim = LaunchConfiguration('use_sim')
 
-    resolution = LaunchConfiguration('resolution', default='0.05')
-    publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
+    cartographer_config_dir = PathJoinSubstitution(
+        [
+            FindPackageShare('turtlebot3_cartographer'),
+            'config',
+        ]
+    )
+    configuration_basename = LaunchConfiguration('configuration_basename')
 
-    rviz_config_dir = os.path.join(get_package_share_directory('turtlebot3_cartographer'),
-                                   'rviz', 'tb3_cartographer.rviz')
+    resolution = LaunchConfiguration('resolution')
+
+    rviz_config_file = PathJoinSubstitution(
+        [
+            FindPackageShare('turtlebot3_cartographer'),
+            'rviz',
+            'cartographer.rviz'
+        ]
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
+            'start_rviz',
+            default_value='true',
+            description='Whether execute rviz2'),
+
+        DeclareLaunchArgument(
+            'use_sim',
+            default_value='false',
+            description='Start robot in Gazebo simulation'),
+
+        DeclareLaunchArgument(
             'cartographer_config_dir',
             default_value=cartographer_config_dir,
-            description='Full path to config file to load'),
+            description='Full direction of config file'),
+
         DeclareLaunchArgument(
             'configuration_basename',
-            default_value=configuration_basename,
+            default_value='turtlebot3_2d.lua',
             description='Name of lua file for cartographer'),
+
         DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
+            'resolution',
+            default_value='0.05',
+            description='Resolution of a grid cell of occupancy grid'),
 
         Node(
             package='cartographer_ros',
             executable='cartographer_node',
-            name='cartographer_node',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time}],
+            parameters=[{'use_sim_time': use_sim}],
             arguments=['-configuration_directory', cartographer_config_dir,
                        '-configuration_basename', configuration_basename]),
 
-        DeclareLaunchArgument(
-            'resolution',
-            default_value=resolution,
-            description='Resolution of a grid cell in the published occupancy grid'),
-
-        DeclareLaunchArgument(
-            'publish_period_sec',
-            default_value=publish_period_sec,
-            description='OccupancyGrid publishing period'),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/occupancy_grid.launch.py']),
-            launch_arguments={'use_sim_time': use_sim_time, 'resolution': resolution,
-                              'publish_period_sec': publish_period_sec}.items(),
-        ),
+        Node(
+            package='cartographer_ros',
+            executable='cartographer_occupancy_grid_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim}],
+            arguments=['-resolution', resolution]),
 
         Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+            arguments=['-d', rviz_config_file],
+            output='screen',
+            condition=IfCondition(start_rviz)),
     ])
